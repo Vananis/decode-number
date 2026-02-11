b266b8f2ca5ede573a3e036410b2145f4a6f148a
vananis <duitle@naver.com>
2026-02-12 01:54:05 +0900

    robots.ts disallow 추가

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["*/about", "*/contact", "*/privacy", "*/terms"],
     },
     sitemap: "https://www.decode-number.com/sitemap.xml",
   }
